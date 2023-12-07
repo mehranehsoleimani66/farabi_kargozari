04f5499a7e51f2986470923512c81ad601da5c87
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-07 22:50:22 +0330

    modified

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,8 @@
+import BannerFarabixo from "./ui/BannerFarabixo";
+import Brokerage from "./ui/brokerage/Brokerage";
 import Card from "./ui/card/Card";
+
+import Describe from "./ui/describe/Describe";
 import Banner from "./ui/homebanner/Banner";
 import Khodro from "./ui/khodrobanner/Khodro";
 
@@ -8,6 +12,9 @@ export default function Home() {
       <Banner />
       <Khodro />
       <Card />
+      <Describe />
+      <BannerFarabixo />
+      <Brokerage />
     </div>
   );
 }
