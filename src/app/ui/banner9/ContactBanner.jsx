570be07f5ecf4e40793a50abf3eee07052b794d6
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-09 16:36:40 +0330

    feat: added contactbanner component

---
+++ src/app/ui/banner9/ContactBanner.jsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+const ContactBanner = () => {
+  return (
+    <div className="flex justify-between items-center w-full h-[200px] py-32 px-32 ">
+      <div className="flex w-1/2 justify-center items-start relative">
+        <Image
+          src="https://irfarabi.com/ic-contact-gray.3cde424c26c7a4bfe7a3.svg"
+          width={50}
+          height={50}
+          className="object-contain"
+        />
+        <div className="absolute flex flex-col justify-start items-start gap-2 ">
+          <span className="text-3xl text-[#066137] font-black">1561</span>
+          <span className="font-normal ">Support@irfarabi.com</span>
+        </div>
+      </div>
+      <div className="w-1/2 flex justify-center items-start">
+        <Image
+          src="https://irfarabi.com/assets/images/icons/ic-contact.svg"
+          width={200}
+          height={200}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ContactBanner;
