f327911eb50e2f6008797e17f43c15d5afe73099
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-12 00:10:35 +0330

    modified

---
+++ src/app/ui/leftModal/LeftModal.jsx
@@ -0,0 +1,11 @@
+import LeftCircle from "./LeftCircle";
+
+const LeftModal = () => {
+  return (
+    <div className=" z-50 fixed left-0 bottom-0 pl-3 pb-3">
+      <LeftCircle />
+      {/* <Modal1 /> */}
+    </div>
+  );
+};
+export default LeftModal;
