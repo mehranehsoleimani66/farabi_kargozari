a0875e40efc599cf26f88973475f8e0381b78b24
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-12 00:09:45 +0330

    modified

--- src/app/page.js
+++ src/app/page.js
@@ -7,21 +7,15 @@ import BusinessPartner from "./ui/banner7/BusinessPartner";
 import LastNews from "./ui/banner8/LastNews";
 import BannerFarabixo from "./ui/banner2/BannerFarabixo";
 import ContactBanner from "./ui/banner9/contactBanner";
-import Modal from "./ui/modal/Modal";
-import Circle from "./ui/modal/Circle";
-import LeftModal from "./ui/modal/LeftModal";
 
 export default function Home() {
   return (
     <div>
       <Banner />
-      <Circle />
-      <Modal />
       <BannerFarabixo />
       <Brokerage />
       <Banner4 />
       <CarBanner />
-      <LeftModal />
       <Describe />
       <BusinessPartner />
       <LastNews />
