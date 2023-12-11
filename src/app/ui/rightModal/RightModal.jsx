ea4598e09adb98b496e14088682446f77cf32b62
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-12 00:11:40 +0330

    modified

---
+++ src/app/ui/rightModal/RightModal.jsx
@@ -0,0 +1,13 @@
+import Circle from "./Circle";
+import Modal2 from "./Modal2";
+
+const RightModal = () => {
+  return (
+    <div className="mr-3 z-50 sticky mb-5">
+      <Circle />
+      <Modal2 />
+    </div>
+  );
+};
+
+export default RightModal;
