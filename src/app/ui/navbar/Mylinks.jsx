55213fc908583e50bbfb4e49a2acf0da676e2721
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-12 00:10:54 +0330

    modified

---
+++ src/app/ui/navbar/Mylinks.jsx
@@ -0,0 +1,210 @@
+export const links = [
+  {
+    name: "مسئولیت اجتماعی",
+    submenu: true,
+    sublinks: [
+      {
+        Head: "Topwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      },
+      {
+        Head: "Bottomwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      },
+      {
+        Head: "innerwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      },
+
+      {
+        Head: "sleepwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      },
+      {
+        Head: "footwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      }
+    ]
+  },
+  {
+    name: "بورس کالا و انرژی",
+    submenu: true,
+    sublinks: [
+      {
+        Head: "Topwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      },
+      {
+        Head: "Bottomwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      },
+      {
+        Head: "innerwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      },
+
+      {
+        Head: "sleepwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      },
+      {
+        Head: "footwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      }
+    ]
+  },
+  {
+    name: "اخبار و تازه ها",
+    submenu: true,
+    sublinks: [
+      {
+        Head: "Topwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      },
+      {
+        Head: "Bottomwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      },
+      {
+        Head: "innerwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      },
+
+      {
+        Head: "sleepwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      },
+      {
+        Head: "footwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" }
+        ]
+      }
+    ]
+  },
+  {
+    name: "اخبار و تازه ها",
+    submenu: true,
+    sublinks: [
+      {
+        Head: "راههای سرمایه گذاری",
+        sublink: [
+          { name: "  خرید سهام", link: "/" },
+          { name: "صندوق سهامی ", link: "/" },
+          { name: "صندوق درآمد ", link: "/" },
+          { name: "(EFT)صندوق سهامی سلام ", link: "/" },
+          { name: "(EFT)صندوق درآمد ثابت قدم  ", link: "/" },
+          { name: " سهام عدالت ", link: "/" }
+        ]
+      },
+      {
+        Head: "شروع سرمایه گذاری",
+        sublink: [
+          { name: "کد بورسی و سجام", link: "/" },
+          { name: "خدمات متمایز فارابی", link: "/" },
+          { name: "انتخاب سرمایه گذاری", link: "/" },
+          { name: "مشاوره تامین مالی", link: "/" }
+        ]
+      },
+      {
+        Head: "ابزارهای سرمایه گذاری",
+        sublink: [
+          { name: "فارابیکسو نکست", link: "/" },
+          { name: "فارابیکسو", link: "/" },
+          { name: "آموزین", link: "/" },
+          { name: "مدیریت دارایی", link: "/" },
+          { name: "باهم", link: "/" },
+          { name: "نکسو", link: "/" }
+        ]
+      }
+    ]
+  }
+];
