16f7b610572467009a8411b896993b5e96be89de
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-12 00:10:14 +0330

    modified

--- src/app/ui/contact/Contact.jsx
+++ src/app/ui/contact/Contact.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 const Contact = () => {
   return (
-    <div className="h-12 bg-gray-50 text-black gap-5 px-4 flex justify-end  text-enter items-center text-base cursor-pointer">
+    <div className="h-12  text-black gap-5 px-4 flex justify-end  text-enter items-center text-base cursor-pointer">
       <Link
         href=""
         className="flex items-center  list-none hover:shadow-sm hover:shadow-gray-300 pr-2 pl-2 h-full"
@@ -10,7 +10,7 @@ const Contact = () => {
       </Link>
       <Link
         href=""
-        className="flex font-bIranZamin items-center list-none hover:shadow-sm hover:shadow-gray-300 h-full pr-2 pl-2"
+        className="flex items-center  list-none hover:shadow-sm hover:shadow-gray-300 pr-2 pl-2 h-full"
       >
         آموزین
       </Link>
