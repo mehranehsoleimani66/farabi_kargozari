fd98a9313854d283796c9e5e5bb39ebf0d750cc6
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-12 13:17:23 +0330

    modified

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
         </div>
       </div>
       {/* AOS Script */}
-      <script src="https://unpkg.com/aos@next/dist/aos.js" async></script>
+      <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
       <script>AOS.init();</script>
     </html>
   );
