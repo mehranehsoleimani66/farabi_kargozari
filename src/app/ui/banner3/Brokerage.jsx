ce105b22f4d5ce9e74a6e32b98070be4ca6300e5
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-15 01:43:00 +0330

    made responsive

--- src/app/ui/banner3/Brokerage.jsx
+++ src/app/ui/banner3/Brokerage.jsx
@@ -2,28 +2,28 @@ import Chart from "../chart/Chart";
 import Link from "next/link";
 const Brokerage = () => {
   return (
-    <div className=" w-full h-[600px] flex justify-between items-center px-32 py-28">
-      <div className="flex w-1/2  justify-center">
+    <div className="flex flex-col-reverse w-full h-[800px] lg:h-[600px] lg:flex lg:flex-row justify-between items-center lg:px-32 lg:py-28">
+      <div className="flex  w-1/2  justify-center items-center ">
         <Chart />
       </div>
 
-      <div className="flex flex-col   flex-1 justify-center items-end text-end gap-8  ">
-        <h6 className="text-[color:var(--text)] font-black text-4xl font-bIranZamin  ">
+      <div className="flex flex-col   flex-1 justify-center items-center text-center lg:items-end lg:text-end gap-8  ">
+        <h6 className="text-[color:var(--text)] font-black text-2xl lg:text-4xl font-bIranZamin  ">
           !یک گام جلوتر از شاخص بورس
         </h6>
-        <p className="text-black font-bold font-bIranZamin  text-xl">
+        <p className="text-black font-bold font-bIranZamin  lg:text-xl">
           .سرمایه‌گذاری را به حرفه‌ای‌ها بسپارید و از افزایش داریی‌تان لذت ببرید
         </p>
-        <div className="flex gap-8 font-bold">
+        <div className="flex flex-col justify-center items-center lg:flex-row gap-8 font-bold">
           <Link
             href="/"
-            className=" border border-gray-300 text-green-600 font-bIranZamin px-3 py-3 rounded-md"
+            className=" border  border-gray-300 text-green-600 font-bIranZamin lg:px-3 lg:py-3 rounded-md"
           >
             صندوق درآمد ثابت
           </Link>
           <Link
             href="/"
-            className=" border border-gray-300 text-green-600 px-3 py-3 font-bIranZamin rounded-md"
+            className=" border  border-gray-300 text-green-600 lg:px-3 lg:py-3 font-bIranZamin rounded-md"
           >
             صندوق سهامی
           </Link>
