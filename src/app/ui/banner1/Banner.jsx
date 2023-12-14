d5c930becc77ee83eb963f79305a8e65b1710f27
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-15 01:42:34 +0330

    made responsive

--- src/app/ui/banner1/Banner.jsx
+++ src/app/ui/banner1/Banner.jsx
@@ -3,27 +3,27 @@ import RightModal from "../rightModal/RightModal";
 
 const Banner = () => {
   return (
-    <div className=" relative flex-1 w-full mt-0 h-[500px]  ">
+    <div className=" relative flex lg:flex-1 lg:w-full mt-0 h-[500px]  ">
       <Image
         src="https://irfarabi.com/assets/images/irfarabi-home-banner2x.webp"
         alt=""
         fill
-        className="object-cover"
+        className="lg:object-cover  "
         priority
       />
-      <div className="flex gap-7 flex-col justify-center items-end absolute top-10 right-14  border-none px-52 py-52">
+      <div className="flex lg:gap-7 flex-col justify-between gap-96 lg:justify-center items-end absolute top-10 right-14  border-none lg:px-52 lg:py-52">
         <h1 className="font-bold text-3xl font-bIranZamin">
           رویا , تغییر , سرمایه گذاری
         </h1>
-        <div className="flex gap-5">
-          <button className="bg-transparent font-bIranZamin border border-gray-400 text-green-700 rounded-md text-base px-7 py-3">
+        <div className="flex  gap-5 w-full">
+          <button className="bg-transparent font-bIranZamin border border-gray-400 text-green-700 rounded-md text-base lg:px-7 lg:py-3">
             ثبت نام در بورس
           </button>
-          <button className="bg-green-700 font-bIranZamin text-white text-base px-5 rounded-md py-3">
+          <button className="bg-green-700 font-bIranZamin text-white text-base lg:px-5 rounded-md lg:py-3">
             ثبت نام غیر حضوری در بورس
           </button>
         </div>
-        <RightModal />
+        {/* <RightModal /> */}
       </div>
     </div>
   );
