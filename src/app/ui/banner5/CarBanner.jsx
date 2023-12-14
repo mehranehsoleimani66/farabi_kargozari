d6a1c2c46693ca97b283c4b35eb3984610e00367
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-15 01:43:26 +0330

    made responsive

--- src/app/ui/banner5/CarBanner.jsx
+++ src/app/ui/banner5/CarBanner.jsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 const CarBanner = () => {
   return (
-    <div className="relative flex-1 w-full h-[calc(100vh-9rem)] ">
-      <Image
+    <div className="relative w-full flex flex-col lg:flex lg:flex-row bg-verticalPic bg-cover bg-no-repeat lg:bg-horizintalPic lg:flex-1  h-[800px] lg:h-[500px] ">
+      {/* <Image
         src="https://irfarabi.com/nexo.2f9b5f28f3f51db1a5ac.png"
         alt=""
         fill
-        className="object-cover"
+        className=""
         priority
-      />
-      <div className="flex gap-7 flex-col justify-center  border border-red-500 items-end absolute top-10 left-7  border-none px-24 py-40">
+      /> */}
+      <div className="flex justify-center items-center gap-7 flex-col    border border-red-500 lg:items-end absolute top-10 left-7  border-none lg:px-24 lg:py-40">
         <h4 className="font-extrabold font-bIranZamin text-3xl">
           {" "}
           خرید خودرو در بورس
@@ -19,7 +19,7 @@ const CarBanner = () => {
           بدون قرعه کشی و با قیمت رقابتی خودروی خود را از طریق بورس کالای
           کارگزاری فارابی خریداری کنید
         </h1>
-        <div className="flex gap-5">
+        <div className="flex flex-col  lg:flex gap-5">
           <button className="bg-green-700 font-bIranZamin text-white text-base px-5 rounded-md py-3">
             دریافت کد بورس کالا
           </button>
