cbbee347a9808d48c74a28121787a470f4f575a9
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-15 15:41:30 +0330

    made responsive

--- src/app/ui/rightModal/Circle.jsx
+++ src/app/ui/rightModal/Circle.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Circle = () => {
   return (
-    <div className=" w-16 h-16 rounded-full text-[color:var(--text)]  border border-[color:var(--text)]  flex justify-center items-center fixed bottom-1 right-2 mr-3 mb-3 bg-white">
+    <div className=" w-16 h-16 rounded-full text-[color:var(--text)]  fixed bottom-1 right-2 border border-[color:var(--text)]  flex justify-center items-center  mr-3 mb-3 bg-white">
       <Image
         src="https://irfarabi.com/assets/images/icons/ic-user-plus.svg"
         width={40}
