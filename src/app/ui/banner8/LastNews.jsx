5048a6b2cf59d48fcb5ed51225e91869e4e65318
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-15 15:40:08 +0330

    made responsive

--- src/app/ui/banner8/LastNews.jsx
+++ src/app/ui/banner8/LastNews.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 const LastNews = () => {
   return (
-    <div className="relative  w-full min-h-[200px] bg-[color:var(--bg)]  ">
+    <div className="relative flex w-full min-h-[200px] bg-[color:var(--bg)]  ">
       <Image
         src="https://irfarabi.com/back-news.f99176000b49ad14753b.png"
         fill
@@ -10,34 +10,32 @@ const LastNews = () => {
         alt=""
         priority
       />
-      <div className="flex justify-between   flex-row-reverse gap-5 items-center  px-32 py-28">
-        <div className=" w-1/3  flex flex-col justify-start items-end gap-12">
-          <h1 className="text-3xl font-bold text-white font-bIranZamin">
-            اطلاعیه و اخبار فارابی
+      <div className="flex-col lg:w-full flex  justify-center  lg:justify-between lg:flex-row-reverse  items-center px-2 lg:px-32 py-28">
+        <div className="lg:w-1/2  flex flex-col justify-center items-center  gap-12">
+          <h1 className="text-3xl font-bold text-white font-bIranZamin text-right">
+            !اطلاعیه و اخبار فارابی
           </h1>
-          <span className="text-white text-2xl  font-bIranZamin ">
+          <span className="text-white text-lg lg:text-2xl  lg:text-right  font-bIranZamin ">
             گزارش امروز بورس
           </span>
+          <Link
+            href="/"
+            className="flex justify-start   text-white bg-[#054326] w-24 text-xl rounded font-bIranZamin py-2 px-2"
+          >
+            ادامه خبر
+          </Link>
         </div>
-        <div className="flex flex-col gap-10 justify-end  items-center w-2/3 min-h-[200px] ">
-          <span className="text-white text-2xl font-bIranZamin ">
-            {" "}
+        <div className="lg:w-1/2  flex flex-col justify-center items-center gap-20">
+          <h1 className="text-3xl font-bold text-white font-bIranZamin lg:text-right"></h1>
+          <span className="text-white text-2xl lg:text-right font-bIranZamin ">
             آغاز معاملات شبانه‌روزی در کارگزاری فارابی
           </span>
-          <div className="flex justify-around w-full  gap-5 items-center">
-            <Link
-              href="/"
-              className=" text-white bg-[#054326] text-xl rounded font-bIranZamin py-2 px-2"
-            >
-              ادامه خبر
-            </Link>
-            <Link
-              href="/"
-              className=" text-white bg-[#054326] text-xl rounded py-2 px-2 font-bIranZamin"
-            >
-              ادامه خبر
-            </Link>
-          </div>
+          <Link
+            href="/"
+            className="flex justify-start  text-center text-white bg-[#054326] w-24 text-xl rounded font-bIranZamin py-2 px-2"
+          >
+            ادامه خبر
+          </Link>
         </div>
       </div>
     </div>
