21d270af4700897374bac00d890e4446a2d6a850
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-15 15:40:51 +0330

    made responsive

--- src/app/ui/footer/Section2.jsx
+++ src/app/ui/footer/Section2.jsx
@@ -6,7 +6,7 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import FacebookIcon from "@mui/icons-material/Facebook";
 const Section2 = () => {
   return (
-    <div className=" w-full  bg-[#1b3e2e] flex justify-between items-center py-5 px-32 ">
+    <div className=" lg:w-full flex-col lg:flex-row bg-[#1b3e2e] flex justify-between items-center py-5 lg:px-32 ">
       <div className="flex flex-1 justify-start item-start ">
         {socialIcons.map((item) => (
           <div
@@ -20,11 +20,11 @@ const Section2 = () => {
           </div>
         ))}
       </div>
-      <div className="flex flex-1 justify-end item-end gap-5 ">
+      <div className="w-full flex flex-col-reverse  lg:flex-1 lg:flex-row  justify-center items-center lg:justify-end item-end gap-5 ">
         <p className="text-[#739385] font-bIranZamin not-italic leading-10 tracking-normal text-sm font-normal">
           | تماس: 1561
         </p>
-        <p className="text-[#739385] not-italic font-bIranZamin leading-10 tracking-normal text-sm font-normal">
+        <p className=" text-[#739385] not-italic font-bIranZamin leading-10 tracking-normal text-sm font-normal">
           © تمامی حقوق برای کارگزاری فارابی محفوظ است
         </p>
       </div>
