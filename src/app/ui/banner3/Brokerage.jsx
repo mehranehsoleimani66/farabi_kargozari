9f7e8244e752b63d7bf4d3b234f3050f8af13144
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-15 15:39:55 +0330

    made responsive

--- src/app/ui/banner3/Brokerage.jsx
+++ src/app/ui/banner3/Brokerage.jsx
@@ -2,12 +2,12 @@ import Chart from "../chart/Chart";
 import Link from "next/link";
 const Brokerage = () => {
   return (
-    <div className="flex flex-col-reverse w-full h-[800px] lg:h-[600px] lg:flex lg:flex-row justify-between items-center lg:px-32 lg:py-28">
-      <div className="flex  w-1/2  justify-center items-center ">
+    <div className="flex flex-col-reverse w-full h-[600px] lg:h-[600px] lg:flex lg:flex-row justify-between items-center lg:px-32 lg:py-28">
+      <div className="flex  w-1/2 h-1/2  justify-center items-center ">
         <Chart />
       </div>
 
-      <div className="flex flex-col   flex-1 justify-center items-center text-center lg:items-end lg:text-end gap-8  ">
+      <div className="flex flex-col h-1/2   lg:flex-1 justify-center items-center text-center lg:items-end lg:text-end gap-8 pt-10 ">
         <h6 className="text-[color:var(--text)] font-black text-2xl lg:text-4xl font-bIranZamin  ">
           !یک گام جلوتر از شاخص بورس
         </h6>
